4cf95fe94f1f80756b24a739f464d8f374983f61
DoE <kdhwh09@gmail.com>
2024-09-17 22:57:55 +0900

    Refactor: 로그인 페이지 회원 & 게스트 로그인 hook 합치기(useLogin) 및 api/user.ts 수정

--- src/apis/api/user.ts
+++ src/apis/api/user.ts
@@ -1,26 +1,13 @@
 import { baseInstance } from 'apis/utils/instance';
 
+// 유저(게스트) 로그인
 export const signIn = async (data: any) => {
   const { email, password } = data;
+  console.log(data);
   try {
-    const { data, status } = await baseInstance.post('/auth/local', { identifier: email, password });
-
-    if (status === 200) {
-      document.cookie = `access_token=${data.jwt}; Max-age=3600; path=/;`;
-    }
-    return status;
-  } catch (error: any) {
-    if (error.response.status === 400) {
-      alert('아이디와 비밀번호를 확인해주세요.');
-    }
-  }
-};
-
-export const guestSignIn = async () => {
-  try {
-    const { data, status } = await baseInstance.post(`/auth/local`, {
-      identifier: 'guest@petmily.com',
-      password: 'asdf1234',
+    const { data, status } = await baseInstance.post('/auth/local', {
+      identifier: email || 'guest@petmily.com',
+      password: password || 'asdf1234',
     });
 
     if (status === 200) {
